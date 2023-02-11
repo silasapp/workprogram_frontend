b2d50d9bea478125b61f8cc1a07af3b72aaba726
frank <silas.o@targetafrica.net>
2023-02-12 00:20:30 +0100

    40

--- src/app/models/step3-budget-proposal.model.ts
+++ src/app/models/step3-budget-proposal.model.ts
@@ -17,7 +17,8 @@ export class budgetProposal {
   total_Company_Expenditure_Dollars: string = '';
 }
 
-export class CAPEX {
+
+export class  capexOpex {
   id: number = 0;
   omL_ID: string = '';
   omL_Name: string = '';
@@ -35,6 +36,88 @@ export class CAPEX {
   dollar: string = '';
   dollar_equivalent = '';
   remarks: string = '';
+}
+
+
+
+
+export class OpexBdy {
+  id: number = 0;
+  omL_ID: string = '';
+  omL_Name: string = '';
+  companyemail: string = '';
+  year_of_WP: string = '';
+  companyName: string = '';
+  companyNumber: number = 0;
+  contract_Type: string = '';
+  terrain: string = '';
+  consession_Type: string = '';
+  company_ID: string = '';
+  item_Type: string = '';
+  item_Description: string = '';
+  naira: string = '';
+  dollar: string = '';
+  dollar_equivalent = '';
+  remarks: string = '';
+}
+
+
+
+
+export class CapexBdy {
+  id: number = 0;
+  omL_ID: string = '';
+  omL_Name: string = '';
+  companyemail: string = '';
+  year_of_WP: string = '';
+  companyName: string = '';
+  companyNumber: number = 0;
+  contract_Type: string = '';
+  terrain: string = '';
+  consession_Type: string = '';
+  company_ID: string = '';
+  item_Type: string = '';
+  item_Description: string = '';
+  naira: string = '';
+  dollar: string = '';
+  dollar_equivalent = '';
+  remarks: string = '';
+}
+
+
+
+
+
+export class CAPEX {
+  id: number = 0;
+  omL_ID: string = '';
+  omL_Name: string = '';
+  companyemail: string = '';
+  year_of_WP: string = '';
+  companyName: string = '';
+  companyNumber: number = 0;
+  contract_Type: string = '';
+  terrain: string = '';
+  consession_Type: string = '';
+  company_ID: string = '';
+  item_Type: string = '';
+
+  acquisition: string = '';
+  processing: string = '';
+  reprocessing: string = '';
+  exploratory_Well_Drilling: string = '';
+  appraisal_Well_Drilling: string = '';
+  development_Well_Drilling: string = '';
+  workover_Operations: string = '';
+  completions: string = '';
+  flowlines: string = '';
+  pipelines: string = '';
+  generators: string = '';
+  turbines_Compressors: string = '';
+  buildings: string = '';
+  other_Equipment: string = '';
+  civil_Works: string = '';
+  other_Costs: string = '';
 }
 
 export class OPEX {
@@ -50,62 +133,9 @@ export class OPEX {
   consession_Type: string = '';
   company_ID: string = '';
   item_Type: string = '';
-  item_Description: string = '';
-  naira: string = '';
-  dollar: string = '';
-  dollar_equivalent = '';
-  remarks: string = '';
+  variable_cost: string = '';
+  fixed_cost: string = '';
+  overheads: string = '';
+  repairs_and_maintenance_cost: string = '';
+  general_expenses: string = '';
 }
-
-// export class CAPEX {
-//   id: number = 0;
-//   omL_ID: string = '';
-//   omL_Name: string = '';
-//   companyemail: string = '';
-//   year_of_WP: string = '';
-//   companyName: string = '';
-//   companyNumber: number = 0;
-//   contract_Type: string = '';
-//   terrain: string = '';
-//   consession_Type: string = '';
-//   company_ID: string = '';
-//   item_Type: string = '';
-
-//   acquisition: string = '';
-//   processing: string = '';
-//   reprocessing: string = '';
-//   exploratory_Well_Drilling: string = '';
-//   appraisal_Well_Drilling: string = '';
-//   development_Well_Drilling: string = '';
-//   workover_Operations: string = '';
-//   completions: string = '';
-//   flowlines: string = '';
-//   pipelines: string = '';
-//   generators: string = '';
-//   turbines_Compressors: string = '';
-//   buildings: string = '';
-//   other_Equipment: string = '';
-//   civil_Works: string = '';
-//   other_Costs: string = '';
-// }
-
-// export class OPEX {
-//   id: number = 0;
-//   omL_ID: string = '';
-//   omL_Name: string = '';
-//   companyemail: string = '';
-//   year_of_WP: string = '';
-//   companyName: string = '';
-//   companyNumber: number = 0;
-//   contract_Type: string = '';
-//   terrain: string = '';
-//   consession_Type: string = '';
-//   company_ID: string = '';
-//   item_Type: string = '';
-
-//   variable_cost: string = '';
-//   fixed_cost: string = '';
-//   overheads: string = '';
-//   repairs_and_maintenance_cost: string = '';
-//   general_expenses: string = '';
-// }
