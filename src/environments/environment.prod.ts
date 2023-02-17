82ffbe15aa2398aa43b90bb64273b94d8e094f8c
TargetAfrica <admin@targetafrica.net>
2023-02-17 13:30:01 +0100

    Deploy 1516

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://workprogramtest.azurewebsites.net/api',
+  apiUrl: 'https://workprogrambackend.azurewebsites.net/api',
   //production: true, apiUrl: 'https://localhost:7129/api',
 
   elpsBase: 'https://elpsdemo.dpr.gov.ng',
