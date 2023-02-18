4ac4db9ed2efbb5ffa197d0e056b728f6bac7b4a
TargetAfrica <admin@targetafrica.net>
2023-02-18 17:24:43 +0100

    Deploy 1526

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'https://workprogrambackend.azurewebsites.net/api',
   //production: true, apiUrl: 'https://localhost:7129/api',
 
-  elpsBase: 'https://elps.nuprc.gov.ng/',
+  elpsBase: 'https://elps.nuprc.gov.ng',
   appid: 'bfbc3f25-be04-440e-9849-ccb609df1be5',
 };
