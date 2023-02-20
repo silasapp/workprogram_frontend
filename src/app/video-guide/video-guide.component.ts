656d1e0941cb7dc541dc18281efcf691dfdfdb82
frank <silas.o@targetafrica.net>
2023-02-20 09:58:21 +0100

    70

--- src/app/video-guide/video-guide.component.ts
+++ src/app/video-guide/video-guide.component.ts
@@ -10,14 +10,7 @@ export class VideoGuideComponent implements OnInit {
   public videoPlayer: any;  
   
   constructor() {  
-    this.videoPlayer = {  
-      options: {  
-        width: '100%',  
-        sources: ['https://dl.infragistics.com/pg/2011-1/web/shared/videoplayer/videos/Infragistics_Presentation_lowRes_1.h264.mp4',  
-          'https://dl.infragistics.com/pg/2011-1/web/shared/videoplayer/videos/Infragistics_Presentation_lowRes_1.webmvp8.webm',  
-          'https://dl.infragistics.com/pg/2011-1/web/shared/videoplayer/videos/Infragistics_Presentation_lowRes_1.theora.ogv']  
-      }  
-    };  
+  
   }  
 
   ngOnInit(): void {
